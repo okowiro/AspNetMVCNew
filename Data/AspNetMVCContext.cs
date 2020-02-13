d00d75e2709ede3a9776384c69cb8e3be9ed5789
Djangmah <Oko.Djangmah@us.gt.com>
2020-02-12 20:07:47 -0600

    Add project files.

---
+++ Data/AspNetMVCContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using AspNetMVC.Models;
+
+namespace AspNetMVC.Data
+{
+    public class AspNetMVCContext : DbContext
+    {
+        public AspNetMVCContext (DbContextOptions<AspNetMVCContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<AspNetMVC.Models.Employee> Employee { get; set; }
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
+
+namespace AspNetMVC.Models
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string Fullname { get; set; }
+        public string Department { get; set; }
+
+        public string Email { get; set; }
+        public string Phone { get; set; }
+    }
+}
